8c71da12238f2d28e6e8e1a2345259760b011585
Jennifer Huynh <jphuy.12@gmail.com>
2023-05-30 14:27:43 -0400

    Commit: Added Call Number + Change Picture

--- app/src/main/java/algonquin/cst2335/huyn0116/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/huyn0116/MainActivity.java
@@ -3,12 +3,47 @@ package algonquin.cst2335.huyn0116;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.w("MainActivity","In onCreate() - Loading Widget");
+        Log.d(TAG,"Message");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.w("MainActivity", "In onStart() - The application is now visible on screen");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.w("MainActivity", "In onResume() - The application is now responding to user input");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.w("MainActivity", "In onPause() - The app is no longer responds to user input");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.w("MainActivity", "In onStop() - The application is no longer visible");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.w("MainActvity", "In onDestroy() - Any memory used by the application is freed.");
     }
 }
