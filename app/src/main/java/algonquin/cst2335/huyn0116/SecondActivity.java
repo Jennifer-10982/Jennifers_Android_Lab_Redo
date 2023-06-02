976a7923d0283a1beba45b214d0f1d305b1eeb72
Jennifer Huynh <jphuy.12@gmail.com>
2023-06-02 18:12:18 -0400

    Commit: Added call feature

--- app/src/main/java/algonquin/cst2335/huyn0116/SecondActivity.java
+++ app/src/main/java/algonquin/cst2335/huyn0116/SecondActivity.java
@@ -3,7 +3,10 @@ package algonquin.cst2335.huyn0116;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -16,10 +19,20 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
 
         TextView text_View = findViewById(R.id.textView);
+        EditText phoneNumber = findViewById(R.id.editTextPhone);
+        Button callbtn = findViewById(R.id.button2);
 
         Intent fromPrevious = getIntent();
         String emailAddress = fromPrevious.getStringExtra("EmailAddress");
 
         text_View.setText("Welcome back: " + emailAddress);
+
+        callbtn.setOnClickListener(clk ->{
+            Intent call = new Intent(Intent.ACTION_DIAL);
+            call.setData(Uri.parse("tel: " + phoneNumber));
+
+            call.putExtra("Phone", phoneNumber.getText().toString());
+            startActivity(call);
+        });
     }
 }
