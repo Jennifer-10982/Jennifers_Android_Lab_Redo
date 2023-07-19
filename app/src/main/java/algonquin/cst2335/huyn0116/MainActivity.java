3e05d90c49eb508184956172e7c31d5c81451629
Jennifer Huynh <jphuy.12@gmail.com>
2023-07-19 16:17:02 -0400

    Commit: Finsihed Week 9

--- app/src/main/java/algonquin/cst2335/huyn0116/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/huyn0116/MainActivity.java
@@ -2,12 +2,37 @@ package algonquin.cst2335.huyn0116;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import algonquin.cst2335.huyn0116.databinding.ActivityMainBinding;
+
 
 /**
  * This page prompts the user to input their password where it will valiadte the input
@@ -19,24 +44,29 @@ import android.widget.Toast;
  */
 public class MainActivity extends AppCompatActivity {
 
-    /**
-     * This holds the text at the centre of the screen
-     */
+    /*
     private TextView textView = null;
 
-    /**
-     * This holds the user input for the password
-     */
     private EditText editText = null;
 
-    /**
-     * This holds the button to validate whether the password meets the requirement
-     */
     private Button btn = null;
+    */
+
+
+    protected String cityName;
+
+//    create a Volley object that will connect to a server
+    protected RequestQueue queue = null;
+
+    private ActivityMainBinding binding;
+
+    Bitmap image;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /*
         setContentView(R.layout.activity_main);
 
         TextView textView = findViewById(R.id.textView);
@@ -47,18 +77,97 @@ public class MainActivity extends AppCompatActivity {
             String password = edittext.getText().toString();
             checkPasswordComplexity(password);
         });
-    }
+        */
+        queue = Volley.newRequestQueue(this);
 
-    ;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-    /**
-     * This function is used to check the complexity of the password where if the password
-     * is missung an Uppercase, a lowercase, a number or a special symbol, an error message
-     * will occur dependent on the missung information
-     *
-     * @param pw The String object that we are checking
-     * @return boolean - true if all tha four requirement is met or false if one of the requirement are not met
-     */
+        binding.forecastButton.setOnClickListener(click -> {
+            cityName = binding.cityTextField.getText().toString();
+            String stringURL="";
+
+            try{
+                stringURL = new StringBuilder()
+                        .append("https://api.openweathermap.org/data/2.5/weather?q=")
+                                .append(URLEncoder.encode(cityName, "UTF-8"))
+                                        .append("&appid=7e943c97096a9784391a981c4d878b22&units=metric").toString();
+            } catch(UnsupportedEncodingException e ){
+                e.printStackTrace();
+            }
+
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET,stringURL,null,
+                    (response)->{
+                            try {
+
+                                JSONObject coord = response.getJSONObject("coord");
+                                JSONArray weatherArray = response.getJSONArray("weather");
+                                JSONObject position0 = weatherArray.getJSONObject(0);
+
+                                String description = position0.getString("description");
+                                String iconName = position0.getString("icon");
+
+                                JSONObject mainObject = response.getJSONObject("main");
+                                double current = mainObject.getDouble("temp");
+                                double min = mainObject.getDouble("temp_min");
+                                double max = mainObject.getDouble("temp_max");
+                                int humidity = mainObject.getInt("humidity");
+
+                                runOnUiThread(() -> {
+                                    binding.temp.setText("The current temperature is " + current);
+                                    binding.temp.setVisibility(View.VISIBLE);
+
+                                    binding.minTemp.setText("The current temperature is " + min);
+                                    binding.minTemp.setVisibility(View.VISIBLE);
+
+                                    binding.maxTemp.setText("The current temperature is " + max);
+                                    binding.maxTemp.setVisibility(View.VISIBLE);
+
+                                    binding.humidity.setText("The current temperature is " + humidity + "%");
+                                    binding.humidity.setVisibility(View.VISIBLE);
+
+                                    binding.icon.setImageBitmap(image);
+                                    binding.icon.setVisibility(View.VISIBLE);
+
+                                    binding.description.setText(description);
+                                    binding.description.setVisibility(View.VISIBLE);
+                                });
+
+
+                                String pathname = getFilesDir() + "/" + iconName + ".png";
+                                File file = new File(pathname);
+                                if (file.exists()) {
+                                    image = BitmapFactory.decodeFile(pathname);
+                                } else {
+                                    ImageRequest imgReq = new ImageRequest("https://openweathermap.org/img/w/" +
+                                            iconName + ".png", new Response.Listener<Bitmap>() {
+                                        @Override
+                                        public void onResponse(Bitmap bitmap) {
+                                            try {
+                                                image = bitmap;
+                                                image.compress(Bitmap.CompressFormat.PNG, 100,
+                                                        MainActivity.this.openFileOutput(iconName + ".png", Activity.MODE_PRIVATE));
+                                                binding.icon.setImageBitmap(image);
+                                            } catch (Exception e) {
+                                                e.printStackTrace();
+                                            }
+                                        }
+                                    }, 1024, 1024, ScaleType.CENTER, null, (error) -> {
+                                        Toast.makeText(MainActivity.this, "" + error, Toast.LENGTH_SHORT).show();
+                                    });
+                                    queue.add(imgReq);
+                                }
+                            } catch(JSONException e ){e.printStackTrace();};
+
+                    },(error)->{});
+                queue.add(request);
+            });
+        }
+}
+
+
+
+    /*
     boolean checkPasswordComplexity(String pw) {
         TextView textView = findViewById(R.id.textView);
         boolean foundUpperCase, foundLowerCase, foundNumber, foundSpecial;
@@ -92,13 +201,7 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    /**
-     * Each of the case is to return true if the character matches any of the cases. If no match
-     * is found, the function will return false
-     *
-     * @param c is the character from the String password
-     * @return boolean dependent if the character matches the special character (TRUE). No match returns false
-     */
+
     boolean isSpecial(char c) {
         switch (c) {
             case '#':
@@ -116,3 +219,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 }
+*/
+
+
